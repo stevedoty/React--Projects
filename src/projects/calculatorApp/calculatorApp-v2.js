916419480d56d831eff039925f7a86af311b8d33
Steven Doty <stevendoty@stevens-mbp.attlocal.net>
2017-07-27 12:50:26 -0700

    calculator version 2.0 update

--- src/projects/calculatorApp/calculatorApp-v2.js
+++ src/projects/calculatorApp/calculatorApp-v2.js
@@ -6,34 +6,55 @@ class CalculatorV2 extends Component{
   constructor(props){
     super(props);
     this.state = {
-      'displayValue':'0'
+      displayValue:0,
+      operandSelected: false,
+      operand: null,
+      previousValue: null
     }
   }
 
+  handleOnClick(key){
+    const { displayValue } = this.state
+    if (key=='AC'){this.setState(
+      {displayValue:0}
+    )}
+    else if (key=='%'){this.setState(
+      {displayValue:((displayValue)/100)}
+    )}
+    else if (key=='+/-'){this.setState(
+      {displayValue: (displayValue * (-1))}
+    )}
+    else {this.setState(
+      {displayValue: String((displayValue + key)*1)}
+    )}
+  }
 
+  handleDot(key){
+    const { displayValue } = this.state
 
+    if( (key==='.') && (displayValue.indexOf('.') === (-1)) ){
+        this.setState( {displayValue: (displayValue + '.') } )
+    }
+  }
 
-handleOnClick(key){
-  if (key=='AC'){this.setState({'displayValue':0})}
-  else if (key=='%'){this.setState({'displayValue':((this.state.displayValue)/100)})}
-  else if (key=='+/-'){this.setState({'displayValue':(this.state.displayValue * (-1))})}
-  else if (key=='9'){this.setState({'displayValue':((this.state.displayValue + '9')*1)})}
-  else if (key=='8'){this.setState({'displayValue':((this.state.displayValue + '8')*1)})}
-  else if (key=='7'){this.setState({'displayValue':((this.state.displayValue + '7')*1)})}
-  else if (key=='6'){this.setState({'displayValue':((this.state.displayValue + '6')*1)})}
-  else if (key=='5'){this.setState({'displayValue':((this.state.displayValue + '5')*1)})}
-  else if (key=='4'){this.setState({'displayValue':((this.state.displayValue + '4')*1)})}
-  else if (key=='3'){this.setState({'displayValue':((this.state.displayValue + '3')*1)})}
-  else if (key=='2'){this.setState({'displayValue':((this.state.displayValue + '2')*1)})}
-  else if (key=='1'){this.setState({'displayValue':((this.state.displayValue + '1')*1)})}
-  else if (key=='0'){this.setState({'displayValue':((this.state.displayValue + '0')*1)})}
-  else if (key=='.'){this.setState({'displayValue':(this.state.displayValue + '.')})}
-  else if (key=='/'){this.setState({'displayValue':this.state.displayValue})}
-  else if (key=='*'){this.setState({'displayValue':'*'})}
-  else if (key=='-'){this.setState({'displayValue':'-'})}
-  else if (key=='+'){this.setState({'displayValue':'+'})}
-  else if (key=='='){this.setState({'displayValue':'='})}
-}
+  handleOperand(operator){
+    const { displayValue, operandSelected, previousValue, operand } = this.state
+
+    this.setState({operandSelected: true, operand: operator, previousValue: displayValue, displayValue: 0});
+  }
+
+  handleEquals(key){
+    const { displayValue, operandSelected, previousValue, operand } = this.state
+
+    if( (key==='=') && (operandSelected: true) ){
+        this.setState(
+          { displayValue: eval(previousValue + operand + displayValue),
+            operandSelected: false,
+            operand: null,
+            previousValue: displayValue })
+    }
+  }
+
   render() {
     return(
       <div className="calculator-container">
@@ -56,15 +77,15 @@ handleOnClick(key){
               <button className="calculator-key key-2" onClick={() => this.handleOnClick(2)}>2</button>
               <button className="calculator-key key-1" onClick={() => this.handleOnClick(1)}>1</button>
               <button className="calculator-key key-0" onClick={() => this.handleOnClick(0)}>0</button>
-              <button className="calculator-key key-dot" onClick={() => this.handleOnClick('.')}>.</button>
+              <button className="calculator-key key-dot" onClick={() => this.handleDot('.')}>.</button>
             </div>
           </div>
           <div className="operator-keys">
-            <button className="calculator-key key-divide" onClick={() => this.handleOnClick('/')}>/</button>
-            <button className="calculator-key key-multiply" onClick={() => this.handleOnClick('*')}>*</button>
-            <button className="calculator-key key-subtract" onClick={() => this.handleOnClick('-')}>-</button>
-            <button className="calculator-key key-add" onClick={() => this.handleOnClick('+')}>+</button>
-            <button className="calculator-key key-equals" onClick={() => this.handleOnClick('=')}>=</button>
+            <button className="calculator-key key-divide" onClick={() => this.handleOperand('/')}>/</button>
+            <button className="calculator-key key-multiply" onClick={() => this.handleOperand('*')}>*</button>
+            <button className="calculator-key key-subtract" onClick={() => this.handleOperand('-')}>-</button>
+            <button className="calculator-key key-add" onClick={() => this.handleOperand('+')}>+</button>
+            <button className="calculator-key key-equals" onClick={() => this.handleEquals('=')}>=</button>
           </div>
         </div>
       </div>
